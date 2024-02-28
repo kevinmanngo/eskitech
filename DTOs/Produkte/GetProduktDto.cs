89d82208f0ca8a7faebee478b3e2ae38f60f070e
kevinmanngo <kevin.magnusson00@gmail.com>
2024-02-28 15:39:37 +0100

    jjsjlkl

---
+++ DTOs/Produkte/GetProduktDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eskitech.Models.Services.ProduktService.DTOs.Produkte
+{
+    public class GetProduktDto
+    {
+        
+    }
+}
